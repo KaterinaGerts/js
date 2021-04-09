e17a0b94df4d9a6709f18c56562464240ecaec07
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-04-09 16:44:02 +0300

    2

--- js/tasks-module3.js
+++ js/tasks-module3.js
@@ -72,22 +72,88 @@
 // countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }) // 3.
 
 
-//МОДУЛЬ 3, ЗАДАЧА №12
+//МОДУЛЬ 3, ЗАДАЧА №13
 
- const apartment = {
-   descr: 'Просторная квартира в центре',
-   rating: 4,
-   price: 2153,
- };
- const values = [];
- // Пиши код ниже этой строки
-const keys = Object.keys(apartment);
-for (let key of keys) {
-  console.log([key]);
-  console.log(apartment[key]);
+//  const apartment = {
+//    descr: 'Просторная квартира в центре',
+//    rating: 4,
+//    price: 2153,
+//  };
+//  const values = [];
+//  // Пиши код ниже этой строки
+// const keys = Object.keys(apartment);
+// for (let key of keys) {
+//   console.log(key);
+//   values.push(apartment[key])
+//   console.log(apartment[key]);
+// }
+
+
+
+
+//  keys['descr', 'rating', 'price'];
+//  values['Просторная квартира в центре', 4, 2153];
+
+//МОДУЛЬ 3, ЗАДАЧА №14
+
+//   function countProps(object) {  
+//     let propCount = 0;
+//     // Пиши код ниже этой строки
+
+//     const keys = Object.keys(object);
+//     for (let key of keys) {
+//       propCount += 1;
+//     }  
+//     console.log(propCount);
+//   // Пиши код выше этой строки
+//   return propCount;
+//   }
+
+
+//  countProps({}) // 0. 
+//  countProps({ name: 'Mango', age: 2 }) // 2. 
+// countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }) // 3.
+ 
+
+//МОДУЛЬ 3, ЗАДАЧА №16
+
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Пиши код ниже этой строки
+//   const keys = Object.values(salaries);  
+//   for (let key of keys) {
+//     totalSalary += key;
+//   }
+//   console.log(totalSalary);
+
+//   // Пиши код выше этой строки
+//   return totalSalary;
+// }
+
+//  countTotalSalary({}) // 0.
+//  countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) // 330.
+//  countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) // 400.
+
+
+//МОДУЛЬ 3, ЗАДАЧА №18
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 9 },
+];
+console.log(products);
+
+function getProductPrice(productName) {
+  // Пиши код ниже этой строки 
+  for (let product of productName) {   
+    if (product === products) {
+      return productName;
+      // console.log(productName);
+    }
+    return null;
+    console.log(null);
+  }  
+  // Пиши код выше этой строки
 }
-
-
-
- keys['descr', 'rating', 'price'];
- values['Просторная квартира в центре', 4, 2153];
